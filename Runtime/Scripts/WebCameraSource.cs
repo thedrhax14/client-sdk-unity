0e43a346a4656f8b2bb9a9fb5680837b7a56d3c6
thedrhax14 <mishamrulla@gmail.com>
2025-06-02 19:45:32 +0400

    Update WebCameraSource.cs

--- Runtime/Scripts/WebCameraSource.cs
+++ Runtime/Scripts/WebCameraSource.cs
@@ -12,21 +12,21 @@ namespace LiveKit
         TextureFormat _textureFormat;
         private RenderTexture _tempTexture;
 
-        public WebCamTexture CamTexture { get; }
+        public WebCamTexture Texture { get; }
 
         public override int GetWidth()
         {
-            return CamTexture.width;
+            return Texture.width;
         }
 
         public override int GetHeight()
         {
-            return CamTexture.height;
+            return Texture.height;
         }
 
         protected override VideoRotation GetVideoRotation()
         {
-            switch (CamTexture.videoRotationAngle)
+            switch (Texture.videoRotationAngle)
             {
                 case 90: return VideoRotation._90;
                 case 180: return VideoRotation._0;
@@ -36,7 +36,7 @@ namespace LiveKit
 
         public WebCameraSource(WebCamTexture texture, VideoBufferType bufferType = VideoBufferType.Rgba) : base(VideoStreamSource.Texture, bufferType)
         {
-            CamTexture = texture;
+            Texture = texture;
             base.Init();
         }
 
@@ -50,7 +50,7 @@ namespace LiveKit
         // Read the texture data into a native array asynchronously
         protected override bool ReadBuffer()
         {
-            if (_reading && !CamTexture.isPlaying)
+            if (_reading && !Texture.isPlaying)
                 return false;
             _reading = true;
             var textureChanged = false;
@@ -67,7 +67,7 @@ namespace LiveKit
                 if (_captureBuffer.IsCreated)
                     _captureBuffer.Dispose();
 
-                var compatibleFormat = SystemInfo.GetCompatibleFormat(CamTexture.graphicsFormat, FormatUsage.ReadPixels);
+                var compatibleFormat = SystemInfo.GetCompatibleFormat(Texture.graphicsFormat, FormatUsage.ReadPixels);
                 _textureFormat = GraphicsFormatUtility.GetTextureFormat(compatibleFormat);
                 _bufferType = GetVideoBufferType(_textureFormat);
 
@@ -75,21 +75,21 @@ namespace LiveKit
                 _previewTexture = new Texture2D(width, height, TextureFormat.BGRA32, false);
                 _captureBuffer = new NativeArray<byte>(width * height * GetStrideForBuffer(_bufferType), Allocator.Persistent);
 
-                if (CamTexture.graphicsFormat != _previewTexture.graphicsFormat)
+                if (Texture.graphicsFormat != _previewTexture.graphicsFormat)
                     _tempTexture = new RenderTexture(width, height, 0, _previewTexture.graphicsFormat);
 
                 textureChanged = true;
             }
 
-            CamTexture.GetPixels32(_readBuffer);
+            Texture.GetPixels32(_readBuffer);
             MemoryMarshal.Cast<Color32, byte>(_readBuffer)
                 .CopyTo(_captureBuffer.AsSpan());
 
             _requestPending = true;
 
-            if (CamTexture.graphicsFormat != _previewTexture.graphicsFormat)
+            if (Texture.graphicsFormat != _previewTexture.graphicsFormat)
             {
-                Graphics.Blit(CamTexture, _tempTexture);
+                Graphics.Blit(Texture, _tempTexture);
                 Graphics.CopyTexture(_tempTexture, _previewTexture);
             }
             else
@@ -98,7 +98,7 @@ namespace LiveKit
                 _previewTexture.SetPixels(CamTexture.GetPixels());
                 _previewTexture.Apply();
 #else
-                Graphics.CopyTexture(CamTexture, _previewTexture);
+                Graphics.CopyTexture(Texture, _previewTexture);
 #endif
             }
 
